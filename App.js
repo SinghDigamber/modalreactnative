15fe17c17d66aeefade6169ee5059011b579bf22
Digamber Singh <digamber@positronx.io>
2020-02-04 03:14:56 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import { 
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Modal
+} from 'react-native';
+
+export default class App extends Component {
+  // initial state
+  state = {
+    isVisible: false
+  };
+
+  // hide show modal
+  displayModal(show){
+    this.setState({isVisible: show})
+  }
+
+  render() {
+    return (
+      <View style = { styles.container }>
+        <Modal
+            animationType = {"slide"}
+            transparent={false}
+            visible={this.state.isVisible}
+            onRequestClose={() => {
+              Alert.alert('Modal has now been closed.');
+            }}>
+
+            <Image 
+              source={require('./assets/scooby.jpeg')}
+              style = { styles.image }/>
+              <Text style = { styles.text }>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  Maecenas eget tempus augue, a convallis velit.</Text>
+
+              <Text 
+                style={styles.closeText}
+                onPress={() => {
+                  this.displayModal(!this.state.isVisible);}}>Close Modal</Text>
+          </Modal>
+            
+          <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                this.displayModal(true);
+              }}>
+              <Text style={styles.buttonText}>Show Modal</Text>
+          </TouchableOpacity>          
+        </View>
+      );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 25,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    display: 'flex',
+    height: 60,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: '#2AC062',
+    shadowColor: '#2AC062',
+    shadowOpacity: 0.5,
+    shadowOffset: { 
+      height: 10, 
+      width: 0 
+    },
+    shadowRadius: 25,
+  },
+  closeButton: {
+    display: 'flex',
+    height: 60,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FF3974',
+    shadowColor: '#2AC062',
+    shadowOpacity: 0.5,
+    shadowOffset: { 
+      height: 10, 
+      width: 0 
+    },
+    shadowRadius: 25,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 22,
+  },
+  image: {
+    marginTop: 150,
+    marginBottom: 10,
+    width: '100%',
+    height: 350,
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 30,
+    padding: 40,
+  },
+  closeText: {
+    fontSize: 24,
+    color: '#00479e',
+    textAlign: 'center',
+  }
+});
---
+++ README.md
@@ -0,0 +1,4 @@
+# modalreactnative
+A comprehensive step by step tutorial on how to work with React Native Modal component. In this tutorial, we will learn how to show essential content such as title, text and image with Modal popup in React Native application.
+
+[React Native Modal Tutorial with Examples](https://www.positronx.io/react-native-modal-tutorial-with-examples/)
